5829053ad3bbf8eeb5f13dfda8676d047283013e
Pezzawinkle <54166225+Pezzawinkle@users.noreply.github.com>
2020-04-14 13:34:50 +1000

    more icon_sizes
    missed a few in the last update

--- Clowns-Science/prototypes/buildings/facility.lua
+++ Clowns-Science/prototypes/buildings/facility.lua
@@ -54,6 +54,7 @@ data:extend(
 			},
 			{
 				icon = "__angelsrefining__/graphics/icons/num_1.png",
+				icon_size=32,
 				tint = {r = 0.8, g = 0.8, b = 0.8, a = 0.5},
 				scale = 0.32,
 				shift = {-12, -12}
@@ -101,6 +102,7 @@ data:extend(
 			},
 			{
 				icon = "__angelsrefining__/graphics/icons/num_3.png",
+				icon_size=32,
 				tint = {r = 0.8, g = 0.8, b = 0.8, a = 0.5},
 				scale = 0.32,
 				shift = {-12, -12}
@@ -125,6 +127,7 @@ data:extend(
 			},
 			{
 				icon = "__angelsrefining__/graphics/icons/num_1.png",
+				icon_size=32,
 				tint = {r = 0.8, g = 0.8, b = 0.8, a = 0.5},
 				scale = 0.32,
 				shift = {-12, -12}
@@ -318,6 +321,7 @@ data:extend(
 			},
 			{
 				icon = "__angelsrefining__/graphics/icons/num_3.png",
+				icon_size=32,
 				tint = {r = 0.8, g = 0.8, b = 0.8, a = 0.5},
 				scale = 0.32,
 				shift = {-12, -12}
