e9c182ca924f804df92d945d4a3a9c2f7c6406cf
Pezzawinkle <54166225+Pezzawinkle@users.noreply.github.com>
2020-08-09 14:24:29 +1000

    Small Fixes
    Platinum issue
    Fluorite issue
    Science pack file moved in omni
    Bumps and publishes

--- Clowns-Extended-Minerals/prototypes/omnicrystals/omnicrystal_updates.lua
+++ Clowns-Extended-Minerals/prototypes/omnicrystals/omnicrystal_updates.lua
@@ -73,59 +73,12 @@ if mods["omnimatter_crystal"] then
   --old additions list, where rec name is [ore.."-pure-processing"]
   local pureOresList = {"chrome", "osmium", "phosphorus", "platinum", "thorium","manganese","magnesium"}
 
-  for _,ore in pairs(pureOresList) do
-    local rec = data.raw.recipe[ore.."-pure-processing"]
-    if rec then
-      local gradeSet = get_grade_set(rec)
-      if not data.raw["item-subgroup"][rec.subgroup.."-omnide"] then
-        local cat = {
-          type = "item-subgroup",
-          name = rec.subgroup.."-omnide",
-          group = "omnicrystal",
-          order = "aa",
-        }
-        toAdd[#toAdd+1]=cat
-      end
-      local ing = table.deepcopy(ingrediences_solvation(rec))
-      local res = table.deepcopy(results_solvation(rec))
-      local ic = salt_omnide_icon(ore.."-ore")
-
-      toAdd[#toAdd+1] = {
-        type = "recipe",
-        name = ore.."-pure-salting",
-        localised_name = {"recipe-name.pure-omnide-salting", {"lookup."..ore}},
-        localised_description = {"recipe-description.pure-omnide-salting", {"lookup."..ore}},
-        category = "omniplant",
-        subgroup = rec.subgroup.."-omnide",
-        enabled = false,
-        ingredients = ing,
-        order = "b[clownsore1-crushed]",
-        icons = ic,
-        icon_size=32,
-        results = res,
-        energy_required = 5,
-      }
-      if gradeSet=="crushed" then
-        omni.lib.add_unlock_recipe("crystallology-1", ore.."-pure-salting")
-      elseif gradeSet=="chunk" then
-        omni.lib.add_unlock_recipe("crystallology-2", ore.."-pure-salting")
-      elseif gradeSet=="crystal" then
-        omni.lib.add_unlock_recipe("crystallology-3", ore.."-pure-salting")
-      elseif gradeSet=="pure" then
-        omni.lib.add_unlock_recipe("crystallology-4", ore.."-pure-salting")
-      elseif ore=="magnesium" then 
-        omni.lib.add_unlock_recipe("crystallology-3", ore.."-pure-salting")
-      end
-    end
-    data:extend(toAdd)
-  end
-  --new additions list where name is clowns-tier-mix#-processing
-  toAdd={} --nil it out
-  for i,tier in pairs(oresGrade) do --grade
-    for j=1,12,1 do --number
-      local rec = data.raw.recipe["clowns-"..tier.."-mix"..j.."-processing"]
-      if rec and rec.normal.results[1].name ~= "angels-void" then
-        local gradeSet = tier
+  if not clowns.special_vanilla then
+    for _,ore in pairs(pureOresList) do
+      if not data.raw.item[ore .."-ore-omnide-salt"] then add_crystal(ore .."-ore",ore:gsub("^%l", string.upper)) end
+      local rec = data.raw.recipe[ore.."-pure-processing"]
+      if rec then
+        local gradeSet = get_grade_set(rec)
         if not data.raw["item-subgroup"][rec.subgroup.."-omnide"] then
           local cat = {
             type = "item-subgroup",
@@ -135,22 +88,15 @@ if mods["omnimatter_crystal"] then
           }
           toAdd[#toAdd+1]=cat
         end
-        --grab ore name from result
         local ing = table.deepcopy(ingrediences_solvation(rec))
         local res = table.deepcopy(results_solvation(rec))
-        local ore = res[1].name or res[1][1]
-        ore=string.sub(ore,1,-13)
-        local lookup = ore
-        if string.find(lookup,"-ore") then
-          lookup=string.sub(lookup,1,-5)
-        end
-        local ic = salt_omnide_icon(ore)
+        local ic = salt_omnide_icon(ore.."-ore")
 
         toAdd[#toAdd+1] = {
           type = "recipe",
-          name = "clowns-"..tier.."-mix"..j.."-pure-salting",
-          localised_name = {"recipe-name.pure-omnide-salting", {"lookup."..lookup}},
-          localised_description = {"recipe-description.pure-omnide-salting", {"lookup."..lookup}},
+          name = ore.."-pure-salting",
+          localised_name = {"recipe-name.pure-omnide-salting", {"lookup."..ore}},
+          localised_description = {"recipe-description.pure-omnide-salting", {"lookup."..ore}},
           category = "omniplant",
           subgroup = rec.subgroup.."-omnide",
           enabled = false,
@@ -161,22 +107,79 @@ if mods["omnimatter_crystal"] then
           results = res,
           energy_required = 5,
         }
-        if tier=="crushed" then
-          omni.lib.add_unlock_recipe("crystallology-1", "clowns-"..tier.."-mix"..j.."-pure-salting")
-        elseif tier=="chunk" then
-          omni.lib.add_unlock_recipe("crystallology-2", "clowns-"..tier.."-mix"..j.."-pure-salting")
-        elseif tier=="crystal" then
-          omni.lib.add_unlock_recipe("crystallology-3", "clowns-"..tier.."-mix"..j.."-pure-salting")
-        elseif tier=="pure" then
-          omni.lib.add_unlock_recipe("crystallology-4", "clowns-"..tier.."-mix"..j.."-pure-salting")
+        if gradeSet=="crushed" then
+          omni.lib.add_unlock_recipe("crystallology-1", ore.."-pure-salting")
+        elseif gradeSet=="chunk" then
+          omni.lib.add_unlock_recipe("crystallology-2", ore.."-pure-salting")
+        elseif gradeSet=="crystal" then
+          omni.lib.add_unlock_recipe("crystallology-3", ore.."-pure-salting")
+        elseif gradeSet=="pure" then
+          omni.lib.add_unlock_recipe("crystallology-4", ore.."-pure-salting")
+        elseif ore=="magnesium" then 
+          omni.lib.add_unlock_recipe("crystallology-3", ore.."-pure-salting")
         end
-        data:extend(toAdd)
       end
-      if mods["angelsrefining"] and settings.startup["angels-salt-sorting"].value then
-        for i, rec in pairs(data.raw.recipe) do
-          if rec.category == "omniplant" and string.find(rec.name,"salting") then
-            omni.lib.replace_recipe_ingredient(rec.name, "hydromnic-acid",{type = "item", name = "omni-catalyst", amount=1})
-            rec.category = "ore-sorting"
+      data:extend(toAdd)
+    end
+    --new additions list where name is clowns-tier-mix#-processing
+    toAdd={} --nil it out
+    for i,tier in pairs(oresGrade) do --grade
+      for j=1,12,1 do --number
+        local rec = data.raw.recipe["clowns-"..tier.."-mix"..j.."-processing"]
+        if rec and rec.normal.results[1].name ~= "angels-void" then
+          local gradeSet = tier
+          if not data.raw["item-subgroup"][rec.subgroup.."-omnide"] then
+            local cat = {
+              type = "item-subgroup",
+              name = rec.subgroup.."-omnide",
+              group = "omnicrystal",
+              order = "aa",
+            }
+            toAdd[#toAdd+1]=cat
+          end
+          --grab ore name from result
+          local ing = table.deepcopy(ingrediences_solvation(rec))
+          local res = table.deepcopy(results_solvation(rec))
+          local ore = res[1].name or res[1][1]
+          ore=string.sub(ore,1,-13)
+          local lookup = ore
+          if string.find(lookup,"-ore") then
+            lookup=string.sub(lookup,1,-5)
+          end
+          local ic = salt_omnide_icon(ore)
+
+          toAdd[#toAdd+1] = {
+            type = "recipe",
+            name = "clowns-"..tier.."-mix"..j.."-pure-salting",
+            localised_name = {"recipe-name.pure-omnide-salting", {"lookup."..lookup}},
+            localised_description = {"recipe-description.pure-omnide-salting", {"lookup."..lookup}},
+            category = "omniplant",
+            subgroup = rec.subgroup.."-omnide",
+            enabled = false,
+            ingredients = ing,
+            order = "b[clownsore1-crushed]",
+            icons = ic,
+            icon_size=32,
+            results = res,
+            energy_required = 5,
+          }
+          if tier=="crushed" then
+            omni.lib.add_unlock_recipe("crystallology-1", "clowns-"..tier.."-mix"..j.."-pure-salting")
+          elseif tier=="chunk" then
+            omni.lib.add_unlock_recipe("crystallology-2", "clowns-"..tier.."-mix"..j.."-pure-salting")
+          elseif tier=="crystal" then
+            omni.lib.add_unlock_recipe("crystallology-3", "clowns-"..tier.."-mix"..j.."-pure-salting")
+          elseif tier=="pure" then
+            omni.lib.add_unlock_recipe("crystallology-4", "clowns-"..tier.."-mix"..j.."-pure-salting")
+          end
+          data:extend(toAdd)
+        end
+        if mods["angelsrefining"] and settings.startup["angels-salt-sorting"].value then
+          for i, rec in pairs(data.raw.recipe) do
+            if rec.category == "omniplant" and string.find(rec.name,"salting") then
+              omni.lib.replace_recipe_ingredient(rec.name, "hydromnic-acid",{type = "item", name = "omni-catalyst", amount=1})
+              rec.category = "ore-sorting"
+            end
           end
         end
       end
