028a5541d86cd3ad87bf4fedfda9c305cb858a6a
Pezzawinkle <54166225+Pezzawinkle@users.noreply.github.com>
2021-03-16 18:51:17 +1100

    icon updates (to match angels new ones)

--- Clowns-Processing/prototypes/recipes/vanilla-recipes.lua
+++ Clowns-Processing/prototypes/recipes/vanilla-recipes.lua
@@ -1,15 +1,4 @@
 local intermediatemulti = angelsmods.marathon.intermediatemulti
---lifted special recipe icon script from angels water treatment
-local function create_recipe_icon(fluid_name, overlay_icon)
-  if not overlay_icon then return angelsmods.functions.get_object_icons(fluid_name) end
-  local icon_layers = util.table.deepcopy(angelsmods.functions.get_object_icons(overlay_icon))
-  for layer_index, layer in pairs(icon_layers) do
-    layer.shift = layer.shift or {}
-    layer.shift = {(layer.shift[1] or 0)/2.3-9, (layer.shift[2] or 0)/2.3-9}
-    layer.scale = (layer.scale or 1)/2.3
-  end
-  return angelsmods.functions.add_icon_layer(angelsmods.functions.get_object_icons(fluid_name), icon_layers)
-end
 
 data:extend(
 {
@@ -565,7 +554,7 @@ data:extend(
   {
     type = "recipe",
     name = "phosphoric-acid-1",
-    icons =angelsmods.functions.create_liquid_recipe_icon({"liquid-phosphoric-acid"}, {{ r = 244, g = 125, b = 001 },{ 242, 242, 242 },{ 214, 012, 012 }}), 
+    icons = angelsmods.functions.create_liquid_recipe_icon({"liquid-phosphoric-acid"}, {{ r = 244, g = 125, b = 001 },{ 242, 242, 242 },{ 214, 012, 012 }}), 
     category = "chemistry",
     subgroup = "clowns-phosphorus",
     order = "b",
@@ -625,22 +614,7 @@ data:extend(
 			{type = "item", name = "solid-salt", amount = 10},
 			{type = "item", name = "magnesium-ore", amount = 5},
 		},
-		icons = {
-			{
-				icon = "__angelsrefining__/graphics/icons/water-saline.png",
-			},
-			{
-				icon = "__angelsrefining__/graphics/icons/solid-salt.png",
-				scale = 0.4,
-				shift = {-8, -8},
-			},
-			{
-				icon = "__Clowns-Processing__/graphics/icons/magnesium-ore.png",
-				scale = 0.4,
-				shift = {8, -8},
-			},
-		},
-		icon_size = 32,
+    icons = angelsmods.functions.create_solid_recipe_icon({"solid-salt"}, "magnesium-ore", {"water-saline"}),
 		order = "f",
 	},
 	{
@@ -659,7 +633,7 @@ data:extend(
 			{type = "item", name = "magnesium-ore", amount = 10},
 		},
 		main_product = "",
-		icons = create_recipe_icon("water-saline", "magnesium-ore"),
+    icons = angelsmods.functions.create_solid_recipe_icon(nil, "magnesium-ore", {"water-saline"}),
 		order = "g",
 	},
   ----------------------------
