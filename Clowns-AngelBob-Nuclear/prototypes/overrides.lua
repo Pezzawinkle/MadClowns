c77a9bcf5432ecb33decdec608c1b87d17602d0c
Pezzawinkle <54166225+Pezzawinkle@users.noreply.github.com>
2021-04-14 00:08:51 +1000

    fixes for industry + ver bump

--- Clowns-AngelBob-Nuclear/prototypes/overrides.lua
+++ Clowns-AngelBob-Nuclear/prototypes/overrides.lua
@@ -7,12 +7,14 @@ if data.raw.item["thorium-fuel-cell"] then
   clowns.functions.replace_ing("thorium-fuel-cell","iron-plate",plate,"ing")
   clowns.functions.replace_ing("thorium-mixed-oxide","iron-plate",plate,"ing")
   clowns.functions.replace_ing("mixed-oxide","iron-plate",plate,"ing")
-  clowns.functions.add_to_table("thorium-nuclear-fuel-reprocessing",{type="item", name=plate, amount=5},"ing")
+  clowns.functions.add_to_table("thorium-nuclear-fuel-reprocessing",{type="item", name=plate, amount=5},"res")
     --table.insert(data.raw.recipe["thorium-nuclear-fuel-reprocessing"].ingredients,{type="item", name=plate, amount=5})
   clowns.functions.replace_ing("radiothermal-fuel","iron-plate",plate,"ing")
 else
 	table.insert(data.raw["technology"]["nuclear-fuel-reprocessing-2"].effects, {type = "unlock-recipe", recipe = "advanced-nuclear-fuel-reprocessing-2"})
 end
 angelsmods.functions.make_void("water-radioactive-waste", "water")
-
-
+if mods["angelsindustries"] then
+  --update nuclear cells
+  clowns.functions.replace_ing("angels-advanced-mixed-oxide-reprocessing","uranium-235","35%-uranium","res")
+end
