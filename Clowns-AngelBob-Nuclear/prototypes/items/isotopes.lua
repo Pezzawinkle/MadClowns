96ee21e40659c9a213379ef99b27cc99747dfef6
Adam Cirillo <adamncirillo@gmail.com>
2026-01-24 15:08:28 +1100

    Disabled unused recipes

--- Clowns-AngelBob-Nuclear/prototypes/items/isotopes.lua
+++ Clowns-AngelBob-Nuclear/prototypes/items/isotopes.lua
@@ -1,41 +1,42 @@
 data:extend(
-{
 	{
-		type = "item",
-		name = "clowns-polonium-210",
-		icon = "__Clowns-AngelBob-Nuclear__/graphics/icons/polonium-210.png",
-		icon_size = 32,
-		subgroup = "clowns-nuclear-isotopes",
-		order = "c",
-		stack_size = 50
-	},
-	{
-		type = "item",
-		name = "clowns-caesium-137",
-		icon = "__Clowns-AngelBob-Nuclear__/graphics/icons/caesium-137.png",
-		icon_size = 32,
-		subgroup = "clowns-nuclear-isotopes",
-		order = "d",
-		stack_size = 50
-	},
-	{
-		type = "item",
-		name = "clowns-strontium-90",
-		icon = "__Clowns-AngelBob-Nuclear__/graphics/icons/strontium-90.png",
-		icon_size = 32,
-		subgroup = "clowns-nuclear-isotopes",
-		order = "e",
-		stack_size = 50
-	},
-	{
-		type = "item",
-		name = "clowns-protactinium-231",
-		icon = "__Clowns-AngelBob-Nuclear__/graphics/icons/protactinium-231.png",
-		icon_size = 32,
-		subgroup = "clowns-nuclear-isotopes",
-		order = "f",
-		stack_size = 50
-	},
+		{
+			type = "item",
+			name = "clowns-polonium-210",
+			icon = "__Clowns-AngelBob-Nuclear__/graphics/icons/polonium-210.png",
+			icon_size = 32,
+			subgroup = "clowns-nuclear-isotopes",
+			order = "c",
+			stack_size = 50
+		},
+		{
+			type = "item",
+			name = "clowns-caesium-137",
+			icon = "__Clowns-AngelBob-Nuclear__/graphics/icons/caesium-137.png",
+			icon_size = 32,
+			subgroup = "clowns-nuclear-isotopes",
+			order = "d",
+			stack_size = 50
+		},
+		{
+			type = "item",
+			name = "clowns-strontium-90",
+			icon = "__Clowns-AngelBob-Nuclear__/graphics/icons/strontium-90.png",
+			icon_size = 32,
+			subgroup = "clowns-nuclear-isotopes",
+			order = "e",
+			stack_size = 50
+		},
+		{
+			type = "item",
+			name = "clowns-protactinium-231",
+			icon = "__Clowns-AngelBob-Nuclear__/graphics/icons/protactinium-231.png",
+			icon_size = 32,
+			subgroup = "clowns-nuclear-isotopes",
+			order = "f",
+			stack_size = 50
+		},
+		--[[ Not used by any recipe
 	{
 		type = "item",
 		name = "clowns-zinc-65",
@@ -54,5 +55,6 @@ data:extend(
 		order = "h",
 		stack_size = 50
 	},
-}
+	]]
+	}
 )
